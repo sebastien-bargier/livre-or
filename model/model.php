1f6601e0a0c601c94ce0585684b8aa5dd7864690
sebastien-bargier <sebastien.bargier@laplateforme.io>
2021-12-09 17:57:33 +0100

    fonction pour enregistrer login et password de l'utilisateur en base de donnée

--- model/model.php
+++ model/model.php
@@ -4,3 +4,12 @@ function dbConnect() {
     $db = mysqli_connect('localhost','root','','livreor');
     return $db;
 }
+
+function insertUser($login, $pwd) {
+    $db = dbConnect();
+    $stmt = mysqli_prepare($db, "INSERT INTO utilisateurs (login, password)
+            VALUES (?, ?)");
+    mysqli_stmt_bind_param($stmt, "ss", $login, $pwd);
+    mysqli_stmt_execute($stmt);
+    return $stmt;
+}

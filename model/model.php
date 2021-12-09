e3cf404a9da239999667f3f0645ec016f859bbc8
sebastien-bargier <sebastien.bargier@laplateforme.io>
2021-12-09 18:04:20 +0100

    fonction pour recuperer les informations utilisateurs

--- model/model.php
+++ model/model.php
@@ -13,3 +13,11 @@ function insertUser($login, $pwd) {
     mysqli_stmt_execute($stmt);
     return $stmt;
 }
+
+function getUtilisateurs() {
+    $db = dbConnect();
+    $sql = "SELECT id,login,password FROM utilisateurs WHERE login='" . htmlentities($_POST['login']) .  "'";
+    $request = mysqli_query($db,$sql);
+    $user = mysqli_fetch_all($request,MYSQLI_ASSOC);
+    return $user;
+}

0700f7c18ca39a1ab3936c7b2c7cd53cd337e1fa
Sebastien Bargier <sebastien.bargier@laplateforme.io>
2021-12-14 20:21:33 +0100

    require profilCTRL.php

--- view/pages/profil.php
+++ view/pages/profil.php
@@ -1,19 +1,21 @@
-<form action='#' method='post'>
+<?php require 'controller/profilCTRL.php'; ?>
+
+<form action='#' method='POST'>
 
     <h1>Mon profil</h1>
 
     <br />
 
     <label for="login">Login</label>
-    <input type="text" name="login" value="<?= $userSession['login']; ?>">
+    <input type="text" name="login" value="<?= $userSession[0]['login']; ?>">
     <p class="error"><?= $msg['login'] ?></p>
 
     <label for="password">Password</label>
-    <input type="password" name="password" value="<?= $userSession['password']; ?>">
+    <input type="password" name="password" value="<?= $userSession[0]['password']; ?>">
     <p class="error"><?= $msg['pwd'] ?></p>
 
     <label for="confirm-password">Confirm password</label>
-    <input type="password" name="confirm-password" value="<?= $userSession['password']; ?>">
+    <input type="password" name="confirm-password" value="<?= $userSession[0]['password']; ?>">
     <p class="error"><?= $msg['c-pwd'] ?></p>
 
     <input type="submit" class="btn" name="mod" value="Modifier"></input>

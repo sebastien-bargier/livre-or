bc831ce7d24888a17920dffbd56544d0f1bfc896
Sebastien Bargier <sebastien.bargier@laplateforme.io>
2021-12-14 20:28:59 +0100

    insertion photo en page d'accueil

--- view/pages/accueil.php
+++ view/pages/accueil.php
@@ -1,3 +1,8 @@
 <div id="container">
-    <a class="github" href="https://github.com/sebastien-bargier/livre-or">Mon lien Github vers livre-or</a>
+    <img id="img-accueil" src="public/img/bmw_custom.jpg" alt="image accueil">
+</div>
+
+<div class="container2">
+    <img class="img-custom" src="public/img/custom1.jpg" alt="image custom1">
+    <img class="img-custom" src="public/img/custom2.jpg" alt="image custom2">
 </div>

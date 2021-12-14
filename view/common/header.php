37a36cfc05550f2e301ea23f8065c15b5db019e9
Sebastien Bargier <sebastien.bargier@laplateforme.io>
2021-12-14 20:29:17 +0100

    menu navigation

--- view/common/header.php
+++ view/common/header.php
@@ -1,6 +1,40 @@
-<nav>
-    <ul>
-        <li><a href="?page=accueil">Acceuil</a></li>
-        <li><a href="?page=inscriptionView">Inscription</a></li>
-    </ul>        
+<div id="header">
+    <div id="header-logo">
+        <h1 id="logo">Custom</h1>
+    </div>
+
+    <div id="header-github">
+        <img id="logo-github" src="public/img/github.png" alt="logo github">
+        <a id="github" href="https://github.com/sebastien-bargier/livre-or">livre-or</a>
+    </div>
+
+<nav role="navigation">
+  <div id="menuToggle">
+    <input type="checkbox" />
+
+        <span>menu</span>
+
+        <ul id="menu"> 
+            
+            <li><a href="?page=accueil">Acceuil</a></li>
+            <?php 
+            
+            if($_SESSION) {
+                echo ('
+                    <li><a href="?page=livre-or">Livre d\'or</a></li>
+                    <li><a href="?page=profil">Profil</a></li>
+                    <li><a href="?page=deconnexion">Déconnexion</a></li>
+                ');
+
+            } else {
+                echo ('
+                    <li><a href="?page=livre-or">Livre d\'or</a></li>
+                    <li><a href="?page=inscription">Inscription</a></li>
+                    <li><a href="?page=connexion">Connexion</a></li>
+                ');
+            }
+
+            ?>
+        </ul> 
+    </div>
 </nav>
